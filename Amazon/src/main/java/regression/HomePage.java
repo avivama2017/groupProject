234472f8371cfe417816ab216e9e5337507cd1fb
mdullah10 <49253687+mdullah10@users.noreply.github.com>
2019-05-28 15:50:52 -0400

    Update My Test Cases

--- Amazon/src/main/java/regression/HomePage.java
+++ Amazon/src/main/java/regression/HomePage.java
@@ -1,40 +1,90 @@
 package regression;
 
 import base.CommonAPI;
-
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebElement;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 public class HomePage extends CommonAPI {
 
     @FindBy(id = "nav-orders")
     WebElement orderTab;
+
     @FindBy(xpath = "//*[@id=\"nav-hamburger-menu\"]/i")
     WebElement hamberger;
+
     @FindBy(id = "twotabsearchtextbox")
     WebElement inputSearch;
+
     @FindBy(xpath = "//*[@id=\"nav-link-prime\"]/span[2]")
     WebElement Tryprime;
-    @FindBy(id = "nav-your-amazon")
+
+    @FindBy(xpath = "//*[@id=\"nav-xshop\"]/a[1]")
     WebElement Todaysdeal;
+
     @FindBy(xpath = "//*[@id=\"nav-link-accountList\"]/span[1]")
     WebElement Signin;
+
     @FindBy(xpath = "//*[@id=\"navFooter\"]/div[1]/div/div[1]/ul/li[1]/a")
     WebElement careers;
+
     @FindBy(xpath = "//*[@id=\"navFooter\"]/div[1]/div/div[1]/ul/li[2]/a")
     WebElement Blog;
+
     @FindBy(xpath = "//*[@id=\"navFooter\"]/div[1]/div/div[1]/ul/li[4]/a")
     WebElement Presscenter;
+
     @FindBy(xpath = "//*[@id=\"nav-logo\"]/a[1]/span[1]")
     WebElement AmazonLogo;
+
     @FindBy(xpath = "//*[@id=\"navFooter\"]/div[1]/div/div[7]/ul/li[8]/a")
     WebElement Help;
+
     @FindBy(xpath = "//*[@id=\"nav-xshop\"]/a[4]")
     WebElement Registry;
 
+    @FindBy(xpath = "//*[@id=\"icp-nav-flyout\"]/span[1]/span[1]/span[1]")
+    WebElement EN;
+
+    @FindBy(xpath = "//*[@id=\"nav-flyout-ya-newCust\"]/a")
+    WebElement NewCoustomer;
+
+    @FindBy(id = "ap_customer_name")
+    WebElement name;
+
+    @FindBy(id = "ap_email")
+    WebElement email;
+
+    @FindBy(id = "ap_password")
+    WebElement pass;
+
+    @FindBy(id = "ap_password_check")
+    WebElement rePass;
+
+    @FindBy(xpath = "//*[@id=\"continue\"]")
+    WebElement CreateAcc;
+
+    @FindBy(css = "#nav-xshop > a:nth-child(6)")
+    WebElement WholeFoods;
+
+    @FindBy(xpath = "//*[@id=\"widgetFilters\"]/div[1]/div[2]/span[8]/div/label/input")
+    WebElement Books;
+
+
+
     public void setOrderTab() {
+
         orderTab.click();
     }
 
@@ -45,7 +95,7 @@ public class HomePage extends CommonAPI {
         }
     }
 
-    public void setinputSerch(String name) throws InterruptedException {
+    public void setInputSearch(String name) throws InterruptedException {
         if (inputSearch.isDisplayed()) {
             inputSearch.sendKeys(name, Keys.ENTER);
         }
@@ -53,7 +103,8 @@ public class HomePage extends CommonAPI {
 
     }
 
-    public void setTryprime() {
+    public void setTryPrime() {
+
         Tryprime.click();
     }
 
@@ -61,7 +112,7 @@ public class HomePage extends CommonAPI {
         Todaysdeal.click();
     }
 
-    public void clickSignin() {
+    public void clickSignIn() {
         Signin.click();
     }
 
@@ -73,11 +124,11 @@ public class HomePage extends CommonAPI {
         Blog.click();
     }
 
-    public void setPresscenter() {
+    public void setPressCenter() {
         Presscenter.click();
     }
 
-    public void clickAamzonLogo() {
+    public void clickAmazonLogo() {
         if (AmazonLogo.isDisplayed()) {
             AmazonLogo.click();
             Assert.assertTrue(true);
@@ -85,15 +136,100 @@ public class HomePage extends CommonAPI {
     }
 
     public void setHelp() {
-        Help.click();
+        if (Help.isDisplayed()) {
+            Help.click();
+            Assert.assertTrue(true);
+            sleepFor(5);
+        }
     }
 
     public void setRegistry() {
         Registry.click();
     }
+
+    public void VerifyDropDownBox() {
+        Select select = new Select(driver.findElement(By.xpath("//*[@id=\"searchDropdownBox\"]")));
+        select.selectByVisibleText("Books");
+        sleepFor(4);
+        driver.findElement(By.xpath("//*[@type='submit']")).click();
+        sleepFor(5);
+    }
+
+    public void setEN() {
+        if (EN.isEnabled()) {
+            EN.click();
+            Assert.assertTrue(true);
+        }
+    }
+
+    public void MouseMovementVerification() {
+        Actions action = new Actions(driver);
+        action.moveToElement(driver.findElement(By.xpath("//*[@id=\"nav-link-accountList\"]/span[2]"))).build().perform();
+        NewCoustomer.click();
+    }
+
+    public void setAccCreate(String Name, String Email, String Pass, String RePass) {
+        Actions action = new Actions(driver);
+        action.moveToElement(driver.findElement(By.xpath("//*[@id=\"nav-link-accountList\"]/span[2]"))).build().perform();
+        NewCoustomer.click();
+
+        name.sendKeys(Name);
+        email.sendKeys(Email);
+        pass.sendKeys(Pass);
+        rePass.sendKeys(RePass);
+        CreateAcc.click();
+        sleepFor(5);
+    }
+
+    public void getScreenShot() {
+        DateFormat dateFormat = new SimpleDateFormat("mm.dd.yyyy-HH:mma");
+        Date date = new Date();
+        String name = dateFormat.format(date);
+        File file = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        try {
+            FileUtils.copyFile(file, new File("src\\main\\resources\\screenshots" + name + "new.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public String navigate() {
+        WholeFoods.click();
+        String before = driver.getTitle();
+        driver.navigate().back();
+        driver.navigate().forward();sleepFor(3);
+        return before;
+    }
+
+    public String getTextByXpath(String locator) {
+        String st = driver.findElement(By.xpath("//*[@id=\"navSwmHoliday\"]/a/img")).getText();
+        return st;
+    }
+    public void setBooks() {
+        Todaysdeal.click();
+        Books.click();
+        sleepFor(4);
+
+    }
+    public List<WebElement> getListOfWebElementsByXpath() {
+        List<WebElement> list = new ArrayList<WebElement>();
+        list = driver.findElements(By.xpath("//*[@id=\"searchDropdownBox\"]"));
+        return list;
+    }
+    public void  ListOfWebElements(){
+        List<WebElement>linkList=driver.findElements(By.tagName("a"));
+        System.out.println(linkList.size());
+    }
+    public List<String> getListOfString(List<WebElement> list) {
+        List<String> items = new ArrayList<String>();
+        for (WebElement element : list) {
+            items.add(element.getText());
+        }
+        return items;
+    }
+
 }
-
-
 
 
 
--- Amazon/src/test/java/RegressionSuite/TestHomePage.java
+++ Amazon/src/test/java/RegressionSuite/TestHomePage.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.support.PageFactory;
 //import org.testng.annotations.AfterMethod;
 
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import regression.HomePage;
@@ -23,69 +24,69 @@ public class TestHomePage extends CommonAPI {
     // public void reCap() {
     //driver.close();
     //}
-    @Test
+    @Test(priority = 1, enabled = false)
     public void clickOnOrder() {
         homePage.setOrderTab();
 
     }
 
-    @Test
+    @Test(priority = 2, enabled = false)
     public void clickOnHamberger() {
         homePage.setHamberger();
     }
 
-    @Test
-    public void serchItems() throws InterruptedException {
-        homePage.setinputSerch("iPhone");
+    @Test(priority = 3, enabled = false)
+    public void searchItems() throws InterruptedException {
+        homePage.setInputSearch("iPhone");
 
     }
 
-    @Test
-    public void clickOnTryprime() {
-        homePage.setTryprime();
+    @Test(priority = 4, enabled = false)
+    public void clickOnTryPrime() {
+        homePage.setTryPrime();
     }
 
-    @Test
+    @Test(priority = 5, enabled = false)
     public void clickOnTodays() {
         homePage.setTodaysdeal();
     }
 
-    @Test
-    public void clickOnSignin() {
-        homePage.clickSignin();
+    @Test(priority = 6, enabled = false)
+    public void clickOnSignIn() {
+        homePage.clickSignIn();
     }
 
-    @Test
+    @Test(priority = 7, enabled = false)
     public void clickOncarrers() {
         homePage.setCareers();
     }
 
-    @Test
+    @Test(priority = 8, enabled = false)
     public void clickOnBlog() {
         homePage.clickOnBlog();
     }
 
-    @Test
-    public void clickOnPreesCenter() {
-        homePage.setPresscenter();
+    @Test(priority = 9, enabled = false)
+    public void clickOnPressCenter() {
+        homePage.setPressCenter();
     }
 
-    @Test
-    public void clickAmazonLogo() {
-        homePage.clickAamzonLogo();
+    @Test(priority = 10, enabled = false)
+    public void clickAmazonLogoTest() {
+        homePage.clickAmazonLogo();
     }
 
-    @Test
-    public void clickFindaGift() {
+    @Test(priority = 11, enabled = false)
+    public void clickHelp() {
         homePage.setHelp();
     }
 
-    @Test
+    @Test(priority = 12, enabled = false)
     public void clickOntRegistry() {
         homePage.setRegistry();
     }
 
-    @Test
+    @Test(priority = 13, enabled = false)
     public void signInValidation() {
         driver.findElement(By.xpath("//*[@id=\"nav-link-accountList\"]/span[1]")).click();
         driver.findElement(By.xpath("//*[@id=\"ap_email\"]")).sendKeys("mdulla1984@gmail.com");
@@ -93,16 +94,91 @@ public class TestHomePage extends CommonAPI {
         driver.findElement(By.xpath("//*[@id=\"signInSubmit\"]")).click();
     }
 
-    @Test
+    @Test(priority = 14, enabled = false)
     public void CartValidation() {
         driver.findElement(By.xpath("//*[@id=\"nav-cart\"]/span[2]")).click();
     }
 
-    @Test
+    @Test(priority = 15, enabled = false)
     public void sellOnAmazonTest() {
         driver.findElement(By.xpath("//*[@id=\"navFooter\"]/div[1]/div/div[3]/ul/li[1]/a")).click();
     }
+
+    @Test(priority = 16, enabled = false)
+    public void AmazonAssistant() {
+        driver.findElement(By.xpath("//*[@id=\"navFooter\"]/div[1]/div/div[7]/ul/li[7]/a")).click();
+
+    }
+
+    @Test(priority = 17, enabled = false)
+    public void accountVarify() {
+        driver.findElement(By.xpath("//*[@id=\"navFooter\"]/div[1]/div/div[7]/ul/li[1]/a")).click();
+    }
+
+    @Test(priority = 18, enabled = false)
+    public void AmazonbusinessCardTest() {
+        driver.findElement(By.xpath("//*[@id=\"navFooter\"]/div[1]/div/div[5]/ul/li[3]/a")).click();
+    }
+
+    @Test(priority = 19, enabled = false)
+    public void PrepaidCardTest() {
+        driver.findElement(By.xpath("//*[@id=\"nav-subnav\"]/a[6]/span")).submit();
+    }
+
+   // @Test
+    public void DropDownBoxValidation() {
+        homePage.VerifyDropDownBox();
+    }
+
+   // @Test
+    public void ClickOnEN() {
+        homePage.setEN();
+    }
+
+    //@Test
+    public void createAccount() {
+
+        homePage.setAccCreate("md", "barackobama90@yahoo.com", "abcd1234", "abcd1234");
+        sleepFor(5);
+    }
+
+    // @Test
+    public void MouseMovementTest() {
+        homePage.MouseMovementVerification();
+        sleepFor(3);
+    }
+
+    //@Test
+    public void TakeScreenShot() {
+        homePage.getScreenShot();
+    }
+
+    //@Test
+    public void checkNevigation() {
+        Assert.assertEquals(homePage.navigate(), "Whole Foods Market @ Amazon.com");
+        System.out.println("Navigate is working");
+    }
+
+    //@Test
+    public void getTextFromWebPage() {
+        homePage.getTextByXpath("Shop Deals of the Day");
+    }
+
+     //@Test
+    public void clickOnBooks() {
+        homePage.setBooks();
+    }
+    //@Test
+    public void ListOFWebElements(){
+        homePage.getListOfWebElementsByXpath();
+    }
+    @Test
+    public void ListOfTotalWebElement(){
+        homePage.ListOfWebElements();
+    }
+
 }
+
 
 
 
